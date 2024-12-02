6971ac279b4693ddb9d6bbee6e3509c7f09d14a5
zedaan <karimzahidjs@gmail.com>
2024-12-03 04:36:12 +0500

    created layout for tranfer order page

---
+++ src/components/Cards/TransferCard.svelte
@@ -0,0 +1,30 @@
+<script>
+    import { Card } from 'flowbite-svelte';
+
+    export let transfers = [
+        {
+            label: "Transfer from",
+            title: "The Friday Night Comedy Show",
+            date: "Friday, November 19, 2023, 8:30pm",
+        },
+        {
+            label: "Transfer to",
+            title: "The Friday Night Comedy Show",
+            date: "Friday, August 16, 2024, 8:30pm",
+        },
+    ];
+</script>
+
+<div class="w-full p-4">
+    <Card class="bg-gray-50 border border-gray-300 rounded-lg w-full" size="auto"  >
+        <div class="flex justify-between flex-wrap p-4">
+            {#each transfers as transfer}
+                <div class="w-1/2">
+                    <p class="text-sm font-normal text-Text-Tartiary">{transfer.label}:</p>
+                    <p class="text-xl font-normal text-Text-Primary">{transfer.title}</p>
+                    <p class="text-sm font-normal text-Text-Tartiary">{transfer.date}</p>
+                </div>
+            {/each}
+        </div>
+    </Card>
+</div>
---
+++ src/pages/transfer/transfer.svelte
@@ -0,0 +1,40 @@
+<script>
+    import TransferCard from "@components/Cards/TransferCard.svelte";
+    import TransferSummary from "@components/Cards/TransferSummary.svelte";
+    const transferData = [
+        {
+            label: "Transfer from",
+            title: "The Friday Night Comedy Show",
+            date: "Friday, November 19, 2023, 8:30pm",
+        },
+        {
+            label: "Transfer to",
+            title: "The Friday Night Comedy Show",
+            date: "Friday, August 16, 2024, 8:30pm",
+        },
+    ];
+    const transferSummaryData = {
+    originalTickets: [
+      { type: 'General Admission', quantity: 2, price: 14.99 },
+      { type: 'VIP Tickets', quantity: 2, price: 24.99 },
+    ],
+    newTickets: [
+      { type: 'General Admission', quantity: 3, price: 12.99 },
+      { type: 'VIP Tickets', quantity: 1, price: 24.99 },
+    ],
+    subtotal: -16.0,
+    tax: -0.96,
+    serviceFee: -0.32,
+    total: -17.38,
+    refund: 17.38,
+  };
+</script>
+
+<div class="w-full">
+    <span class="font-bold text-3xl text-gray-900 p-5">Transfer order</span>
+    <div class="mt-3 w-full flex">
+        <TransferCard {transferData}  />
+        <TransferSummary transferSummary={transferSummaryData} />
+    </div>
+
+</div>
