f74c741f70c86bfb3b39c5d3aea2af8da286e3ed
zedaan <karimzahidjs@gmail.com>
2024-12-04 03:05:58 +0300

    added logic for adding notes

--- src/components/Cards/LeftCard.svelte
+++ src/components/Cards/LeftCard.svelte
@@ -1,7 +1,9 @@
 <script>
   import NoteEdit from "@assets/svg/note-edit.svg";
-  import { Input } from "flowbite-svelte";
+
   import Button from "@components/Button/Button.svelte";
+  import Input from "@components/Input/Input.svelte";
+  import Notedit from "@assets/svg/note-edit-dark.svg";
   export let customerName = "Cameron Williamson";
   export let email = "cameron.williamson@gmail.com";
   export let phone = "910 482 3053";
@@ -11,6 +13,8 @@
   export let purchaseTime = "1:41pm EST";
   export let paymentMethod = "•••• •••• •••• ";
 
+  let submitNote = false;
+
   let isNoteEdit = false;
   let noteEditText = "";
 </script>
@@ -18,7 +22,7 @@
 <div
   class="bg-white shadow-sm rounded-lg p-4 sm:p-6 border border-gray-200 size-full mx-4"
 >
-  <div class="grid grid-cols-2 md:grid-cols-3 gap-6 items-start">
+  <div class="grid grid-cols-2 md:grid-cols-3 gap-10 items-start">
     <div class="space-y-4 text-gray-700">
       <div>
         <span class="text-sm font-normal text-gray-500">Customer name</span>
@@ -64,7 +68,7 @@
         </p>
       </div>
     </div>
-    <div class="col-span- md:col-span3">
+    <div class="">
       <span class="text-sm font-normal text-gray-500 w-full"
         >Payment Method</span
       >
@@ -77,33 +81,59 @@
     </div>
   </div>
 
-  <div class="border-t mt-4 sm:mt-6 pt-4">
-    {#if !isNoteEdit}
+  <div class="border-t mt-4 sm:mt-6 pt-1 md:pt-4">
+    {#if !isNoteEdit && !submitNote}
       <!-- svelte-ignore a11y-click-events-have-key-events -->
       <!-- svelte-ignore a11y-no-static-element-interactions -->
       <div
-        class="flex items-center cursor-pointer mt-2"
+        class="flex items-center cursor-pointer mt-5 md:mt-8"
         on:click={() => (isNoteEdit = true)}
       >
         <img class="h-5 w-5" src={NoteEdit} alt="note-edit" />
         <p class="ml-2 text-primary-500 text-sm">Add order note</p>
       </div>
     {/if}
-    {#if isNoteEdit}
-      <div class="flex items-center w-full gap-3 flex-wrap">
+    {#if !submitNote && isNoteEdit}
+      <div class="flex items-center w-full gap-4 flex-wrap">
         <Input
           type="textarea"
-          size="small"
+          size="full"
           bind:value={noteEditText}
-          class="max-w-md h-10"
+          className="h-14 px-2 leading-5 w-full md:w-96 "
         />
-        <Button className="px-10" size="lg">Add</Button>
-        <Button
-          strokebtn
-          className="px-10 border-none"
-          size="lg"
-          on:click={() => (isNoteEdit = false)}>Cancel</Button
+
+        <div>
+          <Button
+            className="px-12"
+            size="lg"
+            on:click={() => noteEditText && (submitNote = true)}>Add</Button
+          >
+          <Button
+            strokebtn
+            className="px-10 border-none"
+            size="lg"
+            on:click={() => ((isNoteEdit = false), (submitNote = false))}
+            >Cancel</Button
+          >
+        </div>
+      </div>
+    {:else if submitNote && noteEditText}
+      <div class="flex items-center gap-2 py-1 md:pt-3">
+        <img src={Notedit} alt="Note edit" class="shrink-0" />
+        <div class="flex flex-col w-full md:w-96 px-1">
+          <p class="text-sm text-gray-700 break-words">
+            {noteEditText}
+          </p>
+        </div>
+        <button
+          class="text-sm text-primary-500 font-medium mt-2"
+          on:click={() => {
+            noteEditText = noteEditText;
+            submitNote = false;
+          }}
         >
+          Edit
+        </button>
       </div>
     {/if}
   </div>
